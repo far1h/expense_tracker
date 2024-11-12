d9f6fc72e55c413b01cc2dec691a63b596317caf
far1h <farihmuhammad45@gmail.com>
2024-11-12 23:04:27 +0700

    Efficiently Render Long Lists with ListView

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -15,14 +16,15 @@ class _ExpensesState extends State<Expenses> {
     Expense(title: 'Flutter Course', amount: 100, date: DateTime.now(), category: Category.work),
     Expense(title: 'Cinema Course', amount: 50, date: DateTime.now(), category: Category.leisure),
   ];
-  
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('The chart'),
-          Text('Expenses list...'),
+          const Text('The chart'),
+          expenses_list(expenses: _registeredExpenses)
         ],
       ),
     );
