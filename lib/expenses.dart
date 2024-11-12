9e722bfdda4f7723c319d7529467dbc6ed3a5e48
far1h <farihmuhammad45@gmail.com>
2024-11-12 22:31:14 +0700

    Project Setup

---
+++ lib/expenses.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatelessWidget {
+  const Expenses({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('The chart'),
+          Text('Expenses list...'),
+        ],
+      ),
+    );
+  }
+}
