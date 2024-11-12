c1c709bbcfba1d8fd0393bbe1fe21c18f7556f19
far1h <farihmuhammad45@gmail.com>
2024-11-12 22:38:42 +0700

    Add an Expense Data Model with a Unique ID & Explore Initializer Lists

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -7,7 +7,7 @@ class Expenses extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
         children: [
           Text('The chart'),
