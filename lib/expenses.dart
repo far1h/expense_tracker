e696597235b69a64b3c60e21d451e3754d7b9062
far1h <farihmuhammad45@gmail.com>
2024-11-12 22:53:06 +0700

    Convert to stateful and create dummy data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,10 +1,21 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
-class Expenses extends StatelessWidget {
+class Expenses extends StatefulWidget {
   const Expenses({
     super.key,
   });
 
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Flutter Course', amount: 100, date: DateTime.now(), category: Category.work),
+    Expense(title: 'Cinema Course', amount: 50, date: DateTime.now(), category: Category.leisure),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
