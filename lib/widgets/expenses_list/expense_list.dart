240710fed556e4cabee9db7320f6307959c65ace
far1h <farihmuhammad45@gmail.com>
2024-11-14 22:26:48 +0700

    Create a Custom List Item with the Card & Spacer Widgets

--- lib/widgets/expenses_list/expense_list.dart
+++ lib/widgets/expenses_list/expense_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseList extends StatelessWidget {
@@ -10,6 +11,6 @@ class ExpenseList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount: expenses.length, itemBuilder: (ctx, index) => Text(expenses[index].title));
+    return ListView.builder(itemCount: expenses.length, itemBuilder: (ctx, index) => ExpenseItem(expenses: expenses));
   }
 }
